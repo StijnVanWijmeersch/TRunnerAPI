582775ce535cfb133bfa244e81d4bc1f02682d97
dotnovate <stijnvanwm@gmail.com>
2024-08-10 20:03:27 +0200

    Setup the Users module

--- src/Modules/Groups/TRunner.Modules.Groups.Domain/Runners/Runner.cs
+++ src/Modules/Groups/TRunner.Modules.Groups.Domain/Runners/Runner.cs
@@ -6,4 +6,28 @@ public sealed class Runner : Entity
     public Guid Id { get; set; }
     public string UserName { get; set; }
     public int Score { get; set; }
+
+    private Runner() { }
+
+    public static Runner Create(Guid id, string userName, int score)
+    {
+        return new Runner
+        {
+            Id = id,
+            UserName = userName,
+            Score = score
+        };
+    }
+
+    public void UpdateUserName(string userName)
+    {
+        UserName = userName;
+    }
+
+    public void UpdateScore(int score)
+    {
+        Score = score;
+
+        RaiseDomainEvent(new RunnerScoreChangedDomainEvent(Id, score));
+    }
 }
---
+++ src/Modules/Groups/TRunner.Modules.Groups.Domain/Runners/RunnerScoreChangedDomainEvent.cs
@@ -0,0 +1,8 @@
+﻿using TRunner.Common.Domain;
+
+namespace TRunner.Modules.Groups.Domain.Runners;
+public sealed class RunnerScoreChangedDomainEvent(Guid runnerId, int score) : DomainEvent
+{
+    public Guid RunnerId { get; init; } = runnerId;
+    public int Score { get; init; } = score;
+}
